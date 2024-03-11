8de528d1de42174183b799ac100ad20811f4163c
Ali Hasan <73433325+aur0us@users.noreply.github.com>
2024-03-11 13:21:39 +0000

    Prox sensor working

--- Assignment/Assignment.ino
+++ Assignment/Assignment.ino
@@ -1,8 +1,9 @@
 #include <Wire.h>
 #include <Zumo32U4.h>
 
-Zumo32U4LCD lcd;
+Zumo32U4LCD display;
 Zumo32U4LineSensors lineSensors;
+Zumo32U4ProximitySensors proxSensors;
 Zumo32U4Motors motors;
 
 unsigned int lineSensorValues[3];
@@ -29,68 +30,79 @@ void reverse(int duration)
   delay(duration);
 }
 
-void display() {
-  lineSensors.read(lineSensorValues,QTR_EMITTERS_ON);
-  lcd.clear();
-  lcd.gotoXY(2, 0);
-  lcd.print(lineSensorValues[1]); // Middle Sensor
-  lcd.gotoXY(0, 1);
-  lcd.print(lineSensorValues[0]); // Left Sensor
-  lcd.gotoXY(4, 1);
-  lcd.print(lineSensorValues[2]); // Right Sensor
-  delay(20);
-  Serial.println(lineSensorValues[1]);
-  Serial.println(lineSensorValues[0]);
-  Serial.println(lineSensorValues[2]);
-  delay(100);
+void LCDdisplay() {
+  // lineSensors.read(lineSensorValues,QTR_EMITTERS_ON);
+  // display.clear();
+  // display.gotoXY(2, 0);
+  // display.print(lineSensorValues[1]); // Middle Sensor
+  // display.gotoXY(0, 1);
+  // display.print(lineSensorValues[0]); // Left Sensor
+  // display.gotoXY(4, 1);
+  // display.print(lineSensorValues[2]); // Right Sensor
+  // delay(20);
+  // Serial.println(lineSensorValues[1]);
+  // Serial.println(lineSensorValues[0]);
+  // Serial.println(lineSensorValues[2]);
+  // delay(100);
 } 
 
 void setup() {
   lineSensors.initThreeSensors();
+  proxSensors.initThreeSensors();
 }
 
 void loop() {
   lineSensors.read(lineSensorValues, QTR_EMITTERS_ON);
-  display();
+  proxSensors.read();
+  LCDdisplay();
   // Line Avoidance Logic
   if (lineSensorValues[0] > 500 || lineSensorValues[1] > 500 || lineSensorValues[2] > 500) { 
     // Line detected - Turn
 
-    if(lineSensorValues[0] > 400)
+    if(lineSensorValues[0] > 400) // Left sensor turn right
     {
-        turn(1, 100, 200); // Turn right (adjust direction if needed)
+        turn(1, 100, 125); // Turn right (adjust direction if needed)
     }
 
-    if(lineSensorValues[1] > 400)
+    if(lineSensorValues[2] > 400)  // Right sensor turn left
     {
-      // int duration = reverseCounter >= 3 ? 300 : 150 ;
-      reverse(300);
-    }
-    if(lineSensorValues[2] > 400)
-    {
-      turn(0 ,100,200); // Turn Left
+      turn(0 ,100,125); // Turn Left
     }
 
-     if(lineSensorValues[2] > 400 && lineSensorValues[1] > 400)
+     if(lineSensorValues[2] > 400 && lineSensorValues[1] > 400) // Right and middle, reverse and turn left
     {
       reverse(150);
-      turn(0 ,100,200); // Turn left
+      turn(0 ,100,125); // Turn left
     }
 
-     if(lineSensorValues[0] > 400 && lineSensorValues[1] > 400)
+     if(lineSensorValues[0] > 400 && lineSensorValues[1] > 400) // Left and middle, reverse and turn right
     {
       reverse(150);
-      turn(1 ,100,200); // Turn right
+      turn(1 ,100,125); // Turn right
     }
 
-    if(lineSensorValues[0] > 400 && lineSensorValues[2] > 400)
+    if(lineSensorValues[0] > 400 && lineSensorValues[2] > 400) // Right and left, reverse and turn 
     {
       reverse(150);
-      turn(1,100,200);
+      turn(1,100,125);
     }
     
+     if(lineSensorValues[1] > 400) // Middle sensor reverse
+    {
+      reverse(300);
+    }
   } else {  
+  if(proxSensors.countsFrontWithLeftLeds() >= 6
+        || proxSensors.countsFrontWithRightLeds() >= 6)
+        {
+          motors.setSpeeds(0, 0);
+          display.gotoXY(1, 0);
+          display.print("HOUSE");
+        }
+        else{
+        display.clear();  
+        motors.setSpeeds(75, 75); 
+        }
     // No line detected - Move forward
-    motors.setSpeeds(75, 75); 
   }
 }
